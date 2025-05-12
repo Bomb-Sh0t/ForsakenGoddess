c1929acd6806126deb328a24ddc21e14ba917167
Bomb_Sh0t <bombshotyt@gmail.com>
2025-05-12 18:26:23 -0400

    Corrected item stack sizes

--- src/main/java/com/bombshot/forsakengoddess/item/ModItems.java
+++ src/main/java/com/bombshot/forsakengoddess/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.bombshot.forsakengoddess.item;
 
 import com.bombshot.forsakengoddess.ForsakenGoddess;
+import net.minecraft.world.item.BookItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -12,17 +13,17 @@ public class ModItems {
     public static final DeferredItem<Item> FORSAKEN_INGOT = ITEMS.register("forsaken_ingot",
             () -> new Item(new Item.Properties()));
 
-    public static final DeferredItem<Item> FATE_OF_THE_GODDESS = ITEMS.register("fate_of_the_goddess",
-            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<BookItem> FATE_OF_THE_GODDESS = ITEMS.register("fate_of_the_goddess",
+            () -> new BookItem(new Item.Properties().stacksTo(1)));
 
     public static final DeferredItem<Item> PADLOCK_KEY = ITEMS.register("padlock_key",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1)));
 
     public static final DeferredItem<Item> EYE_OF_THE_GODDESS = ITEMS.register("eye_of_the_goddess",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(16)));
 
     public static final DeferredItem<Item> LORE_TABLET = ITEMS.register("lore_tablet",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(16)));
 
     public static final DeferredItem<Item> GODDESS_SMITHING_TEMPLATE = ITEMS.register("goddess_smithing_template",
             () -> new Item(new Item.Properties()));
